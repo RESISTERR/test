2e718946e68e91d7b4da296b2c4ae10010cf62ee
thp1997 <tirthpatel49@gmail.com>
2017-12-29 16:35:32 +0000

    spes: Compile HWUI for Better Performance
    Signed-off-by: smokey18 <kmboplays@gmail.com>
    Signed-off-by: agus404  <agustk666@gmail.com>
    Change-Id: Ie818061ab61079b598daa19939747eb23bbc171d

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -120,6 +120,9 @@ endif
 ODM_MANIFEST_SKUS += k7tn
 ODM_MANIFEST_K7TN_FILES := $(DEVICE_PATH)/configs/hidl/manifest_k7tn.xml
 
+# HWUI
+HWUI_COMPILE_FOR_PERF := true
+
 # Init
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):init_xiaomi_spes
 TARGET_RECOVERY_DEVICE_MODULES := init_xiaomi_spes
