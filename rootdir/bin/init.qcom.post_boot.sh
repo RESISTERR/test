ac34162d6f06cb9d67d3ced86618647f863cbb22
Hoang Pham Anh Duy <duy@etylix.me>
2022-10-01 13:26:54 +0700

    spes: clean up performance tunning
    Signed-off-by: Hoang Pham Anh Duy <duy@etylix.me>
    Change-Id: I436639c8db50246f763ba80714c941374f189ce3

--- rootdir/bin/init.qcom.post_boot.sh
+++ rootdir/bin/init.qcom.post_boot.sh
@@ -112,61 +112,54 @@ echo N > /sys/module/lpm_levels/system/perf/perf-l2-gdhs/suspend_enabled
 setprop vendor.post_boot.parsed 1
 
 # power/perf tunings for khaje
-# Core control parameters on big
-echo 0 > /sys/devices/system/cpu/cpu0/core_ctl/enable
-echo 2 > /sys/devices/system/cpu/cpu4/core_ctl/min_cpus
-echo 40 > /sys/devices/system/cpu/cpu4/core_ctl/busy_down_thres
-echo 60 > /sys/devices/system/cpu/cpu4/core_ctl/busy_up_thres
-echo 100 > /sys/devices/system/cpu/cpu4/core_ctl/offline_delay_ms
-echo 4 > /sys/devices/system/cpu/cpu4/core_ctl/task_thres
+case "$soc_id" in
+          "518" )
 
-# Setting b.L scheduler parameters
-echo 65 > /proc/sys/kernel/sched_downmigrate
-echo 71 > /proc/sys/kernel/sched_upmigrate
-echo 85 > /proc/sys/kernel/sched_group_downmigrate
-echo 100 > /proc/sys/kernel/sched_group_upmigrate
-echo 1 > /proc/sys/kernel/sched_walt_rotate_big_tasks
+    # Core control parameters on big
+    echo 0 > /sys/devices/system/cpu/cpu0/core_ctl/enable
+    echo 2 > /sys/devices/system/cpu/cpu4/core_ctl/min_cpus
+    echo 40 > /sys/devices/system/cpu/cpu4/core_ctl/busy_down_thres
+    echo 60 > /sys/devices/system/cpu/cpu4/core_ctl/busy_up_thres
+    echo 100 > /sys/devices/system/cpu/cpu4/core_ctl/offline_delay_ms
+    echo 4 > /sys/devices/system/cpu/cpu4/core_ctl/task_thres
 
-# configure governor settings for little cluster
-echo "schedutil" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-echo 0 > /sys/devices/system/cpu/cpu0/cpufreq/schedutil/up_rate_limit_us
-echo 0 > /sys/devices/system/cpu/cpu0/cpufreq/schedutil/down_rate_limit_us
-echo 1516800 > /sys/devices/system/cpu/cpu0/cpufreq/schedutil/hispeed_freq
-echo 691200 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
-echo 0 > /sys/devices/system/cpu/cpu0/cpufreq/schedutil/rtg_boost_freq
+    # Setting b.L scheduler parameters
+    echo 65 > /proc/sys/kernel/sched_downmigrate
+    echo 71 > /proc/sys/kernel/sched_upmigrate
+    echo 85 > /proc/sys/kernel/sched_group_downmigrate
+    echo 100 > /proc/sys/kernel/sched_group_upmigrate
+    echo 1 > /proc/sys/kernel/sched_walt_rotate_big_tasks
 
-# configure governor settings for big cluster
-echo "schedutil" > /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor
-echo 0 > /sys/devices/system/cpu/cpu4/cpufreq/schedutil/up_rate_limit_us
-echo 0 > /sys/devices/system/cpu/cpu4/cpufreq/schedutil/down_rate_limit_us
-echo 1344000 > /sys/devices/system/cpu/cpu4/cpufreq/schedutil/hispeed_freq
-echo 1056000 > /sys/devices/system/cpu/cpu4/cpufreq/scaling_min_freq
-echo 0 > /sys/devices/system/cpu/cpu4/cpufreq/schedutil/rtg_boost_freq
+    # configure governor settings for little cluster
+    echo "schedutil" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+    echo 0 > /sys/devices/system/cpu/cpu0/cpufreq/schedutil/rtg_boost_freq
 
-echo "0:1190000" > /sys/devices/system/cpu/cpu_boost/input_boost_freq
-echo 120 > /sys/devices/system/cpu/cpu_boost/input_boost_ms
-echo "0:1900800 1:0 2:0 3:0 4:2400000 5:0 6:0 7:0" > /sys/devices/system/cpu/cpu_boost/powerkey_input_boost_freq
-echo 400 > /sys/devices/system/cpu/cpu_boost/powerkey_input_boost_ms
+    # configure governor settings for big cluster
+    echo "schedutil" > /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor
+    echo 0 > /sys/devices/system/cpu/cpu4/cpufreq/schedutil/rtg_boost_freq
 
-# sched_load_boost as -6 is equivalent to target load as 85. It is per cpu tunable.
-echo -6 >  /sys/devices/system/cpu/cpu0/sched_load_boost
-echo -6 >  /sys/devices/system/cpu/cpu1/sched_load_boost
-echo -6 >  /sys/devices/system/cpu/cpu2/sched_load_boost
-echo -6 >  /sys/devices/system/cpu/cpu3/sched_load_boost
-echo -6 >  /sys/devices/system/cpu/cpu4/sched_load_boost
-echo -6 >  /sys/devices/system/cpu/cpu5/sched_load_boost
-echo -6 >  /sys/devices/system/cpu/cpu6/sched_load_boost
-echo -6 >  /sys/devices/system/cpu/cpu7/sched_load_boost
-echo 85 > /sys/devices/system/cpu/cpu0/cpufreq/schedutil/hispeed_load
-echo 85 > /sys/devices/system/cpu/cpu4/cpufreq/schedutil/hispeed_load
+    echo "0:1190000" > /sys/devices/system/cpu/cpu_boost/input_boost_freq
+    echo 120 > /sys/devices/system/cpu/cpu_boost/input_boost_ms
+    echo "0:1516800 1:0 2:0 3:0 4:1766400 5:0 6:0 7:0" > /sys/devices/system/cpu/cpu_boost/powerkey_input_boost_freq
+    echo 400 > /sys/devices/system/cpu/cpu_boost/powerkey_input_boost_ms
 
-# Set Memory parameters
-configure_memory_parameters
+    # sched_load_boost as -6 is equivalent to target load as 85. It is per cpu tunable.
+    echo -6 >  /sys/devices/system/cpu/cpu0/sched_load_boost
+    echo -6 >  /sys/devices/system/cpu/cpu1/sched_load_boost
+    echo -6 >  /sys/devices/system/cpu/cpu2/sched_load_boost
+    echo -6 >  /sys/devices/system/cpu/cpu3/sched_load_boost
+    echo -6 >  /sys/devices/system/cpu/cpu4/sched_load_boost
+    echo -6 >  /sys/devices/system/cpu/cpu5/sched_load_boost
+    echo -6 >  /sys/devices/system/cpu/cpu6/sched_load_boost
+    echo -6 >  /sys/devices/system/cpu/cpu7/sched_load_boost
+    echo 85 > /sys/devices/system/cpu/cpu0/cpufreq/schedutil/hispeed_load
+    echo 85 > /sys/devices/system/cpu/cpu4/cpufreq/schedutil/hispeed_load
 
-# Enable bus-dcvs
-for device in /sys/devices/platform/soc
-do
-    for cpubw in $device/*cpu-cpu-ddr-bw/devfreq/*cpu-cpu-ddr-bw
+    # Set Memory parameters
+    configure_memory_parameters
+
+    # Enable bus-dcvs
+    for device in /sys/devices/platform/soc
     do
         echo "bw_hwmon" > $cpubw/governor
         echo 50 > $cpubw/polling_interval
