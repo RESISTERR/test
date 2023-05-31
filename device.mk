f68974decfc8e79d88a9a38b0da8e0f787a40954
klozz <carlosj@klozz.dev>
2023-05-31 04:42:52 +0000

    spes: Switch fp to 2.3
     * Xiaomi/hardware doesnt have 2.1 version
    
    Change-Id: I2489a1f8c681bf0415b1847056474c780e613bfc
    Signed-off-by: klozz <carlosj@klozz.dev>

--- device.mk
+++ device.mk
@@ -255,7 +255,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service.xiaomi
+    android.hardware.biometrics.fingerprint@2.3-service.xiaomi
 
 # FM
 PRODUCT_PACKAGES += \
