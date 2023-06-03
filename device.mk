88498942e894dc380e5853ded12c382667e9f658
(klozz) <carlosj@klozz.dev>
2023-06-03 09:52:48 +0000

    spes: Upgrade usb to 1.3
    Change-Id: Ia35496b506fe55881749b42d24dbd151e1460de2

--- device.mk
+++ device.mk
@@ -491,7 +491,7 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.2-service-spes
+    android.hardware.usb@1.3-service-qti
 
 # Vendor service manager
 PRODUCT_PACKAGES += \
